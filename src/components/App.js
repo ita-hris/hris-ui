de6754cbd333a6114005f2a4a670fc49e58d0cd8
Yauheniya Kustreyeva <Yauheniya.Kustreyeva@itechart-group.com>
2020-01-23 17:40:04 +0300

    Added a button that sends a request to the backend

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
 import '../styles/App.css';
+import { get } from '../services/service.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.sendRequest = this.sendRequest.bind(this);
+    }
+
+    sendRequest() {
+        //TODO Replace string with env var process.env.REACT_APP_API_URI
+        get("http://localhost:8080" + '/init', (response) => {
+            console.log("response", response);
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello!</h1>
+                <button onClick={this.sendRequest}>Send request</button>
             </div>
         );
     }
